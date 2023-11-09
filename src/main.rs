d15323d8f6205ce2f0fea2876028972be5349ae3
Storgisl <antipov03306@list.ru>
2023-11-10 00:01:52 +0300

    commit

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,14 @@
-use bevy::prelude::*;
-use bevy::input::mouse::MouseMotion;
-use bevy::pbr::wireframe::WireframePlugin;
+use bevy::{prelude::*,
+    input::mouse::MouseMotion,
+    pbr::wireframe::{NoWireframe, Wireframe, WireframeColor, WireframeConfig, WireframePlugin},
+    render::{
+        render_resource::WgpuFeatures,
+        settings::{RenderCreation, WgpuSettings},
+        RenderPlugin,
+    },
+};
+#[derive(Component)]
+struct WireframeButton;
 
 #[derive(Component)]
 struct Camera {
@@ -8,9 +16,29 @@ struct Camera {
 }
 fn main() {
     App::new()
-        .add_plugins((DefaultPlugins, WireframePlugin))
+        .add_plugins((
+            DefaultPlugins.set(RenderPlugin {
+                render_creation: RenderCreation::Automatic(WgpuSettings {
+                    // WARN this is a native only feature. It will not work with webgl or webgpu
+                    features: WgpuFeatures::POLYGON_MODE_LINE,
+                    ..default()
+                }),
+            }),
+            // You need to add this plugin to enable wireframe rendering
+            WireframePlugin,
+        ))
+        // Wireframes can be configured with this resource. This can be changed at runtime.
+        .insert_resource(WireframeConfig {
+            // The global wireframe config enables drawing of wireframes on every mesh,
+            // except those with `NoWireframe`. Meshes with `Wireframe` will always have a wireframe,
+            // regardless of the global configuration.
+            global: true,
+            // Controls the default color of all wireframes. Used as the default color for global wireframes.
+            // Can be changed per mesh using the `WireframeColor` component.
+            default_color: Color::WHITE,
+        })
         .add_systems(Startup, setup)
-        .add_systems(Update, (camera_movement, camera_rotation))
+        .add_systems(Update, (camera_movement, camera_rotation, update_colors))
         .run();
 }
 
@@ -19,6 +47,9 @@ fn setup(
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
+    // wireframe 
+    commands.spawn(ButtonBundle::default())
+    .insert(WireframeButton);
     // circular base
     commands.spawn(PbrBundle {
         mesh: meshes.add(shape::Circle::new(4.0).into()),
@@ -43,10 +74,20 @@ fn setup(
         transform: Transform::from_xyz(4.0, 8.0, 4.0),
         ..default()
     });
+    // camera
     commands.spawn(Camera3dBundle {
         transform: Transform::from_xyz(0.0, 2.0, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
         ..Default::default()
     }).insert(Camera { speed: 5.0 });
+    // text
+    commands.spawn(
+        TextBundle::from_section("", TextStyle::default()).with_style(Style {
+            position_type: PositionType::Absolute,
+            top: Val::Px(10.0),
+            left: Val::Px(10.0),
+            ..default()
+        }),
+    );
 }
 
 fn camera_movement(
@@ -96,6 +137,54 @@ fn camera_rotation(
 
             transform.rotation = yaw_rotation * transform.rotation;
             transform.rotation = transform.rotation * pitch_rotation;
+        }
+    }
+}
+
+fn update_colors(
+    keyboard_input: Res<Input<KeyCode>>,
+    mut config: ResMut<WireframeConfig>,
+    mut wireframe_colors: Query<&mut WireframeColor>,
+    mut text: Query<&mut Text>,
+) {
+    text.single_mut().sections[0].value = format!(
+        "
+Controls
+---------------
+Z - Toggle global
+X - Change global color
+C - Change color of the green cube wireframe
+
+WireframeConfig
+-------------
+Global: {}
+Color: {:?}
+",
+        config.global, config.default_color,
+    );
+
+    // Toggle showing a wireframe on all meshes
+    if keyboard_input.just_pressed(KeyCode::Z) {
+        config.global = !config.global;
+    }
+
+    // Toggle the global wireframe color
+    if keyboard_input.just_pressed(KeyCode::X) {
+        config.default_color = if config.default_color == Color::WHITE {
+            Color::PINK
+        } else {
+            Color::WHITE
+        };
+    }
+
+    // Toggle the color of a wireframe using WireframeColor and not the global color
+    if keyboard_input.just_pressed(KeyCode::C) {
+        for mut color in &mut wireframe_colors {
+            color.color = if color.color == Color::GREEN {
+                Color::RED
+            } else {
+                Color::GREEN
+            };
         }
     }
 }
